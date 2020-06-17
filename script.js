a79ecebd45063f1c0b1b7aebd2d4aaf4e83b1cf5
GorginZ <georgia.z.leng@gmail.com>
2020-06-17 16:04:25 +1000

    colour random rgb function and querySelector event

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const button = document.querySelector('button');
 const body = document.querySelector('body');
 
-
-
+function randCol(min, max) { 
+    const col1 = Math.floor(Math.random() * (max - min + 1) + min)
+    const col2 = Math.floor(Math.random() * (max - min + 1) + min)
+    const col3 = Math.floor(Math.random() * (max - min + 1) + min)
+    const rgbString = '\'rgb('
+    const myCol =  rgbString + col1 + ',' + col2 + ',' +  col3 + ')\';'
+    return myCol
+  }
     button.addEventListener("click", (e) => {
-        document.body.style.background = "blue"
+        document.body.style.background = randCol(0,255);
+        console.log(randCol(0,255))
+
     })
 
-
-
-//     // Nics code
-// const button = document.querySelector("button");
-
-// button.addEventListener("click", function() {
-//     const curColour = document.body.style.backgroundColor;
-
-//     if (curColour === 'red') {
-//         document.body.style.backgroundColor = "blue";
-//     }
-//     else {
-//         document.body.style.backgroundColor = "red";
-//     }
-// });
+const rgbString = 'rgb('
+const myCol =  rgbString + col1 + ',' + col2 + ',' +  col3 + ')';
