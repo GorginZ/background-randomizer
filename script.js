499b4b48334a5f062b35d284e58d69f813a2c6e5
GorginZ <georgia.z.leng@gmail.com>
2020-06-17 10:37:33 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+
+
+
+    button.addEventListener("click", (e) => {
+        document.body.style.background = "blue"
+    })
+
+
+
+//     // Nics code
+// const button = document.querySelector("button");
+
+// button.addEventListener("click", function() {
+//     const curColour = document.body.style.backgroundColor;
+
+//     if (curColour === 'red') {
+//         document.body.style.backgroundColor = "blue";
+//     }
+//     else {
+//         document.body.style.backgroundColor = "red";
+//     }
+// });
